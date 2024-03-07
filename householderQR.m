f3e9a6b0e4ef583c95b4cfac3d8c6f5547a369d3
Alessandro <Alessandro@DESKTOP-8I79BSP.station>
2024-03-07 18:26:08 +0100

    Miglioramento di householderQR

--- householderQR.m
+++ householderQR.m
@@ -1,23 +1,23 @@
-%Generazione matrice randomica
+% Generazione matrice randomica
 A = randi([1, 10], 5, 5);
 [Qmatlab, Rmatlab] = qr(A);
 
-%Inizializzazione di R e determinazione delle dimensioni
+% Inizializzazione di R e determinazione delle dimensioni
 R = A;
 [m, n] = size(A);
 c = min(m, n);
-%Memorizzo tutte le matrici di householder all'interno di un array
+% Memorizzo tutte le matrici di householder all'interno di un array
 H = cell(1, c);
 
-%Applicazione della fattorizzazione QR
+% Applicazione della fattorizzazione QR
 for k = 1:c
-    x = R(k:m, k); %Corrisponde ad a_k
-    e = [1; zeros(length(x)-1, 1)]; %Vettore nullo eccetto prima componente
+    x = R(k:m, k); % Corrisponde ad a_k
+    e = [1; zeros(length(x)-1, 1)]; % Vettore nullo eccetto prima componente
     sigma = norm(x); %Calcolo della norma 2 di x
-    %sign(x(1)) per ottimizzare il risultato
+    % sign(x(1)) per ottimizzare il risultato
     vk = sign(x(1)) * sqrt(sigma * (sigma + abs(x(1)))) * e + x;
 
-    %Calcolo della matrice di householder H_k
+    % Calcolo della matrice di householder H_k
     %hk = eye(length(x)) - 2 * (vk * vk') / (sigma * (sigma + abs(x(1))));
     hk = eye(length(x)) - 2 * (vk * vk') / (vk' * vk);
     if k > 1
@@ -25,24 +25,30 @@ for k = 1:c
     end
 
     H{k} = hk;
-    %Annullo gli elementi sotto la diagonale della colonna k di R
+    % Annullo gli elementi sotto la diagonale della colonna k di R
     R = hk * R;
 end
 
-%Inizializzazione di Q con la prima matrice H
+% Inizializzazione di Q con la prima matrice H
 Q = H{1};
 for i = 2:c
-    %Calcolo Q moltiplicando riga per colonna tutte le H
+    % Calcolo Q moltiplicando riga per colonna tutte le H
     Q = H{i} * Q;
 end
 
-%Troncamento delle matrici Q e R del 80%
-perc_troncamento = 0.2;
-num_col_troncate = round(size(Q, 2) * perc_troncamento);
-num_row_troncate = round(size(R, 1) * perc_troncamento);
+% Troncamento delle matrici Q e R del 80%
+compression_ratio = 0.2;
+num_col_troncate = round(size(Q, 2) * compression_ratio);
+num_row_troncate = round(size(R, 1) * compression_ratio);
 
 Q_troncata = Q(:, 1:num_col_troncate);
 R_troncata = R(1:num_row_troncate, :);
 
-%Ricostruzione dell'immagine compressa
-immagine_compressa = Q_troncata * R_troncata;
+% Ricostruzione dell'immagine compressa
+A_k = round(Q_troncata * R_troncata);
+
+for i = 1:m
+    for j = 1:n
+        A_k(i, j) = abs(A_k(i, j));
+    end
+end
