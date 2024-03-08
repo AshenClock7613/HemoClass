d0511a7fdfb6568be502a9e18ac689f0629a9e55
Antonio <anto-spe@live.it>
2024-03-08 23:02:06 +0100

    prova

--- testFunzioni.m
+++ testFunzioni.m
@@ -1,4 +1,5 @@
-A = randi([1, 10], 5, 5);
+y = func_imread('Immagini/');
+view_images(y)
 compression_ratio = 0.8; % 80% di compressione
-A_k_QR = func_householderQR(A, compression_ratio);
-A_k_SVD = func_ourSVD(A, 2);
+A_k_QR = func_householderQR(y{1}, compression_ratio);
+A_k_SVD = func_ourSVD(y{1}, 2);
