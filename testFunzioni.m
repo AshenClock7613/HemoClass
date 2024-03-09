d93b6b43d50fe09623232c8385574b865e6cfe25
Antonio <anto-spe@live.it>
2024-03-09 15:51:40 +0100

    aggiunti

--- testFunzioni.m
+++ testFunzioni.m
@@ -1,5 +1,5 @@
 y = func_imread('Immagini/');
 view_images(y)
-compression_ratio = 0.8; % 80% di compressione
+ compression_ratio = 0.8; % 80% di compressione
 A_k_QR = func_householderQR(y{1}, compression_ratio);
 A_k_SVD = func_ourSVD(y{1}, 2);
