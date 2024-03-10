ef4811bf6b5576385b591cc465ed0c56f33d375e
Ale Pico <alexanpicone@gmail.com>
2024-03-10 17:23:28 +0100

    corretto qrEigen

--- qrEigen.m
+++ qrEigen.m
@@ -10,9 +10,9 @@ eigenvector = eye(m);
     for k = 1:nIter
         [Q, R] = func_getQR(M);
         % Aggiornamento di M con sottomatrice RQ
-        M = R * Q';
+        M = R * Q;
         % Calcolo autovettore
-        eigenvector = eigenvector * Q';
+        eigenvector = eigenvector * Q;
     end
     % Estrazione autovalore
     eigenvalue = diag(A);
