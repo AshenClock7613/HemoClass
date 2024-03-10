3ada43e6e641ca73b558d22294eeea9911ad8c32
Ale Pico <alexanpicone@gmail.com>
2024-03-10 17:15:37 +0100

    Aggiornato

--- testFunzioni.m
+++ testFunzioni.m
@@ -1,7 +1,8 @@
 y = func_imread('Immagini/');
 compression_ratio = 0.8; % 80% di compressione
 A_k_QR = func_householderQR(double(y{1,1}), compression_ratio);
-A_k_SVD = func_ourSVD(double(y{1,1}), 50);+
+A_k_SVD = func_ourSVD(double(y{1,1}), 50);
+[autovalori, autovettori] = qrEigen(double(y{1,1}), 5);
 % Conversione double uint8
 figure;
 imshow(y{1,1});
