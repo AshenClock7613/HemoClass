bf479cdacfd19628e21b5121907a7607c5928f32
Alessandro <Alessandro@DESKTOP-8I79BSP.station>
2024-03-11 23:07:21 +0100

    Aggiornatto qrSVD

---
+++ func_qrSVD.m
@@ -0,0 +1,24 @@
+function A_k = func_qrSVD(A, k)
+    [m, n] = size(A);
+
+    R1 = A;
+    U = eye(m);
+    V = eye(n);
+
+    for i = 1 : k
+        [Q1, R1] = func_getQR(R1);
+        [Q2, R2] = func_getQR(R1');
+        R1 = R2';
+
+        if m <= n
+            U = U * Q1;
+            V = V * Q2;
+        else
+            U = Q1 * U;
+            V = Q2 * V;
+        end
+    end
+
+    S = R1;
+    A_k = U * S * V';
+end
--- testFunzioni.m
+++ testFunzioni.m
@@ -1,13 +1,11 @@
 y = func_imread('Immagini/');
-compression_ratio = 0.8; % 80% di compressione
-A_k_QR = func_householderQR(double(y{1,1}), compression_ratio);
 A_k_SVD = func_ourSVD(double(y{1,1}), 50);
-autovalori = qrEigen2(double(y{1,1}), 150);
+autovalori =func_qrSVD(double(y{1,1}), 20);
 % Conversione double uint8
 figure;
 imshow(y{1,1});
 figure;
-imshow(uint8(A_k_QR));
+imshow(uint8(A_k_SVD));
 figure;
 imshow(uint8(autovalori));
 
